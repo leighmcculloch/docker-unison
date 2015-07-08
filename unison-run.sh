8b9d1cd0bb9ec0f006fa2da0d093b502d9905079
Sal Poliandro <popsikle@gmail.com>
2015-07-08 16:45:07 -0400

    unison-fsmonitor support

--- unison-run.sh
+++ unison-run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 export HOME=/root
 cd /unison
+if [ -f /bin/unison-$UNISON_VERSION-fsmonitor ]; then
+  ln -s -f /bin/unison-$UNISON_VERSION-fsmonitor /bin/unison-fsmonitor
+fi
 exec unison-$UNISON_VERSION -socket 5000 >>/var/log/unison.log 2>&1
